3382b9c63e7acf37927def656fdbd758fcf3b6f6
NachoToast <32235595+NachoToast@users.noreply.github.com>
2025-06-14 13:52:18 +1200

    first commit

---
+++ Source/LessSadNightOwls/1.5/LessSadNightOwls.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace PharmacistFix;
+
+[StaticConstructorOnStartup]
+public class LessSadNightOwls
+{
+    private static readonly HashSet<TimeAssignmentDef> suppressedAssignments = [
+        TimeAssignmentDefOf.Sleep,
+        TimeAssignmentDefOf.Joy,
+        TimeAssignmentDefOf.Anything];
+
+    static LessSadNightOwls()
+    {
+        Harmony harmony = new("NachoToast.LessSadNightOwls");
+
+        harmony.Patch(
+            original: AccessTools.Method(
+                type: typeof(ThoughtWorker_IsDayForNightOwl),
+                name: "CurrentStateInternal"),
+            postfix: new HarmonyMethod(
+                methodType: typeof(LessSadNightOwls),
+                methodName: nameof(CurrentStateInteral_Postfix)));
+    }
+
+    private static void CurrentStateInteral_Postfix(Pawn p, ref ThoughtState __result)
+    {
+        if (!__result.Active)
+        {
+            // already inactive
+            return;
+        }
+
+        if (p.IsPrisoner)
+        {
+            // prisoner - suppress
+            __result = false;
+            return;
+        }
+
+        TimeAssignmentDef assignment = p.timetable?.CurrentAssignment;
+
+        if (assignment != null && suppressedAssignments.Contains(assignment))
+        {
+            // suppressable assignment
+            __result = false;
+        }
+    }
+}
